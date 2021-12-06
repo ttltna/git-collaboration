b34e37101d05617c4488d2f36052c0d48729ee41
Blackraven93 <61782746+Blackraven93@users.noreply.github.com>
2021-12-06 14:42:31 +0900

    Revert "feat : make calculate function and execute (#8)" (#9)
    This reverts commit 58ba6c674884a1bee0e63de3a96df3d7d3176622.

--- 전다원/calculate.js
+++
@@ -1,9 +0,0 @@
-let add=(a, b)=>a + b;
-let subtract=(a ,b)=>a - b;
-let multiplay=(a, b)=>a * b;
-let divide=(a, b)=>a / b;
-
-console.log(add(1,2));
-console.log(subtract(10,2));
-console.log(multiplay(9,9));
-console.log(divide(10,2));
