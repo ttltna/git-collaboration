bd785206781bed62488d591c52291841fc300a2f
ttltna <86463041+ttltna@users.noreply.github.com>
2021-12-06 14:02:43 +0900

    [계산식 기능] 김태원 미션 제출합니다. (#7)
    * feat : 더하기 빼기 곱하기 나누기를 화살표표기법 매소드로 구현하기
    
    * feat : 사칙연산함수 화살표표기법으로 구현하기
    
    * refactor : Number함수 제거및 return 제거
    
    * refactor : 함수선언 소문자로 바꾸기
    
    * docs : 폴더만들어서 파일넣기
    
    * style : 폴더만들어서 파일넣기

---
+++ 김태원/calculate.js
@@ -0,0 +1,9 @@
+let add=(a, b)=>a + b;
+let subtract=(a ,b)=>a - b;
+let multiplay=(a, b)=>a * b;
+let divide=(a, b)=>a / b;
+
+console.log(add(100,5));
+console.log(subtract(230,86));
+console.log(multiplay(15,19));
+console.log(divide(10,2));
