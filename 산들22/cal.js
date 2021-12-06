afdfb8d40f0c58277bff7a1a84ff4f647622fcd8
sandeulju <94872442+sandeulju@users.noreply.github.com>
2021-12-06 19:50:50 +0900

    feat : twotwo (#18)
    Co-authored-by: DawonJeon <dawon3024@naver.com>

---
+++ 산들22/cal.js
@@ -0,0 +1,10 @@
+
+let add=(a, b)=>a + b;
+let subtract=(a ,b)=>a - b;
+let multiplay=(a, b)=>a * b;
+let divide=(a, b)=>a / b;
+
+console.log(add(1,5));
+console.log(subtract(10,2));
+console.log(multiplay(15,19));
+console.log(divide(10,2));
