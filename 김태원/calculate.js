f38f7178815f9bd36c0c207bd513b8ff409b9841
ttltna <ttltnaaa@gmail.com>
2021-12-07 15:00:02 +0900

    feat : 제곱함수 추가

--- 김태원/calculate.js
+++ 김태원/calculate.js
@@ -2,8 +2,10 @@ let add=(a, b)=>a + b;
 let subtract=(a ,b)=>a - b;
 let multiplay=(a, b)=>a * b;
 let divide=(a, b)=>a / b;
+let square=(a,b)=>a ** b;
 
 console.log(add(100,5));
 console.log(subtract(230,86));
 console.log(multiplay(15,19));
 console.log(divide(10,2));
+console.log(square(2,6));
