d9f6b17512e1ee7e4bed5a696c2a5e0175c4d152
VeryverySoHappy <frontEnd0815@gmail.com>
2021-12-07 19:01:13 +0900

    feat: first try

---
+++ 이주희/calculate.js
@@ -0,0 +1,15 @@
+// Add (덧셈)
+const add = (a, b) => a + b
+console.log(add(10, 5))
+
+// Subtract (뺄셈)
+const sub = (a, b) => a - b
+console.log(sub(10, 5))
+
+// Multiply (곱셈)
+const mul = (a, b) => a * b
+console.log(mul(10, 5))
+
+// Divide (나눗셈)
+const div = (a, b) => a / b
+console.log(div(10, 5))
