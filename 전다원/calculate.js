38bea2b2ddb81debe2097af202a265ede6f95cce
DawonJeon <dawon3024@naver.com>
2021-12-07 11:31:27 +0900

    feat: pull from blackraven and push

--- 전다원/calculate.js
+++ 전다원/calculate.js
@@ -1,10 +1,23 @@
 
-let add=(a, b)=>a + b;
-let subtract=(a ,b)=>a - b;
-let multiplay=(a, b)=>a * b;
-let divide=(a, b)=>a / b;
+function add(a, b) {
+    var test1= a + b;
+    return test1; 
+}
+function subtract(a ,b) {
+    var test2= a - b;
+    return test2; 
+}
+function multiplay (a, b) {
+    var test3= a * b;
+    return test3;
+}
+function divide(a, b) {
+    var test4 = a/b;
+    return test4
+}
 
-console.log(add(1,4));
-console.log(subtract(2,2));
-console.log(multiplay(1,19));
+
+console.log(add(1,2));
+console.log(subtract(10,1));
+console.log(multiplay(9,9));
 console.log(divide(10,2));
