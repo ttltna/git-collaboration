cf14170607a3c8e6adb06448e9b4c8aa56c43068
Dawpm <94824394+DawonJeon@users.noreply.github.com>
2021-12-07 11:42:37 +0900

    feat : change parameter and execute function (#22)
    hotfix : resolve conflict

--- 전다원/calculate.js
+++ 전다원/calculate.js
@@ -4,6 +4,7 @@ const multiplay=(a, b) => a * b;
 const divide=(a, b)=> a / b;
 
 
+
 console.log(add(4, 6));
 
 console.log(subtract(10, 5));
@@ -11,3 +12,4 @@ console.log(subtract(10, 5));
 console.log(multiplay(4,7));
 
 console.log(divide(4, 2));
+
