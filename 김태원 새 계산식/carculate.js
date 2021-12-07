a5814974c344ba38f8ba963a68fa8bb52f4d1bfe
ttltna <86463041+ttltna@users.noreply.github.com>
2021-12-07 18:59:50 +0900

    [새 계산식]김태원 새 미션 제출합니다. (#24)
    * feat : 새로만든 사칙연산식
    
    * style : readme파일 추가

---
+++ 김태원 새 계산식/carculate.js
@@ -0,0 +1,25 @@
+function add(a, b){
+    return a+b;
+}
+
+function sub(a, b){
+    return a-b;
+}
+
+function mul(a, b){
+    return a*b;
+}
+
+function div(a, b){
+    return a/b;
+}
+
+function squ(a, b){
+    return a**b;
+}
+
+console.log(add(55,44));
+console.log(sub(33,22));
+console.log(mul(11,9));
+console.log(div(8,7));
+console.log(squ(6,5));
