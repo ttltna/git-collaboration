0cd076a6bf0fdcdf5da2882fcbbd331131e36e06
Blackraven93 <reblackraven@gmail.com>
2021-12-07 10:56:08 +0900

    test conflict

--- 전다원/calculate.js
+++ 전다원/calculate.js
@@ -1,10 +1,13 @@
+const add = (a, b)=> a + b;
+const subtract = (a ,b)=> a - b;
+const multiplay=(a, b) => a * b;
+const divide=(a, b)=> a / b;
 
-let add=(a, b)=>a + b;
-let subtract=(a ,b)=>a - b;
-let multiplay=(a, b)=>a * b;
-let divide=(a, b)=>a / b;
 
-console.log(add(1,5));
-console.log(subtract(10,2));
-console.log(multiplay(15,19));
-console.log(divide(10,2));
+console.log(add(4, 6));
+
+console.log(subtract(10, 5));
+
+console.log(multiplay(4,7));
+
+console.log(divide(4, 2));
